1ed17900ae9e4222ebf3ad3adf5923ed8e967772
yqureishy <yqureishy1993@gmail.com>
2021-11-10 12:45:16 -0600

    initial commit

---
+++ DollarsBankCoreJavaAppV1/src/main/java/com/dollarsbank/application/DollarsBankCoreJavaAppV1Application.java
@@ -0,0 +1,13 @@
+package com.dollarsbank.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsBankCoreJavaAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsBankCoreJavaAppV1Application.class, args);
+	}
+
+}
---
+++ DollarsBankCoreJavaAppV1/src/test/java/com/dollarsbank/application/DollarsBankCoreJavaAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollarsbank.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarsBankCoreJavaAppV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
